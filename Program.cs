04832a262a5e102625616c3a2b2eebfab6890d63
rune0018 <rune00nielsen@gmail.com>
2021-08-09 08:44:05 +0200

    kommentar

--- Program.cs
+++ Program.cs
@@ -11,22 +11,14 @@ namespace GetEtTal
             {
                 Getettal.start();
             }
-            //Rul en tærning og få et tilfældigt tal
-            //int guessThisNumber = Dice.Roll();
-            //Console.Write("Gæt et tal: ");
-
-            ////Gør plads
-            //int inputnumber = 0;
-            //int.TryParse(Console.ReadLine(), out inputnumber);
-
-            ////Feedback til brugeren
-            //Console.Write("Du gættede " + inputnumber);
-            //Console.WriteLine(Getettal.Get(inputnumber));
         }
-
+        //tærningen der bliver brugt til at gøre tallet random
         class Dice
         {
+            //holder på default siderne på tærningen
             public static int Sides = 6;
+
+            //kaster tærningen og retunere tallet
             public static int Roll()
             {
                 Random random = new Random();
@@ -37,20 +29,26 @@ namespace GetEtTal
         {
             public static int guessThisNumber;
             public static int inputnumber;
-            public static void start()
+            public static void start() //staten af kode
             {
                 Gui.clear();
-                string navn = Gui.GetStringFromUser("hvad hedder du: ");
+
+                //bruger gui klassen til at hente navnet fra brugeren
+                string navn = Gui.GetStringFromUser("hvad hedder du"); 
                 Gui.Write("hej " + navn);
+
+                //ser om det er muligt at lave svaret om til et tal
                 int.TryParse(Gui.GetStringFromUser(" hvor mange sider vil du have på din tærning: "), out Dice.Sides);
                 guessThisNumber = Dice.Roll();
 
+                //kører indtil svaret matcher computerens tal
                 while (Guess(inputnumber) != " og det var rigtigt")
                 {
-                    int.TryParse(Gui.GetStringFromUser("Gæt et tal: "),out inputnumber);
+                    int.TryParse(Gui.GetStringFromUser("Gæt et tal"),out inputnumber);
                     Gui.Write(" "+Guess(inputnumber)+" ");
                 }
             }
+            // gæt håndtering for at få feedback på hvad brugeren skrev
             public static string Guess(int inputnumber)
             {
                 if (inputnumber > guessThisNumber)
@@ -69,16 +67,19 @@ namespace GetEtTal
         }
         class Gui
         {
+            //samle input fra brugeren op og sender det tilbagge fra hvor det kom fra og skriver i consollen med en guide givet ved kald
             public static string GetStringFromUser(string guide)
             {
-                Console.Write(guide);
+                Console.Write(guide + ": ");
                 string input = Console.ReadLine();
                 return input;
             }
+            //skriver i consollen
             public static void Write(string input)
             {
                 Console.Write(input);
             }
+            //clear consollen
             public static void clear()
             {
                 Console.Clear();
